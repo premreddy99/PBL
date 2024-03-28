0ca7fdac35f930466dcccb94e780d9033dc6d77a
Navya0709 <maidharkarinavya@gmail.com>
2024-03-28 12:21:36 +0530

    update

---
+++ tests/StudentManagement.UnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+using Xunit;
+using StudentManagementAPI.Controllers;
+
+namespace StudentManagement.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void GiveStudent_ReturnsExpectedString()
+        {
+            // Arrange
+            var controller = new StudentController();
+
+            // Act
+            var result = controller.GiveStudent();
+
+            // Assert
+            Assert.Equal("abc", result);
+        }
+    }
+}
